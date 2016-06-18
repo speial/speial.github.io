8d205c87093486c0c08664c41764613ba54abe6b
speial <->
2016-06-18 14:16:04 +0200

    Changed some stuff

--- cms/create.php
+++ cms/create.php
@@ -6,17 +6,38 @@ $f='index';
 
 $dirs = array_filter(glob('../2*'), 'is_dir');
 
-$dir=$dirs[4];
+foreach ($dirs as $dir)
+{
+	echo "Processing: $dir ";
+	$new_page=createPage($dir,$f);
+	if (isset($new_page['info']))
+	{
+		echo ($new_page['info']);
+	}
+	else
+	{
+		$old_page=file_get_contents("$dir/$f.html");
+		if ($new_page['page']===$old_page)
+		{
+			echo "No difference";
+		}
+		else
+		{
+			file_put_contents("$dir/$f.html",$new_page);
+			echo "UPDATED";
+		}
+	}
+	echo "\n";
+}
 
-// print_r($dirs); die();
+die();
 
-$page=createPage($dir,$f);
-file_put_contents("$dir/$f.html",$page);
 
 echo json_last_error();
 
 function createPage($d,$f)
 {
+	if (!file_exists("$d/$f.json")) return ['info'=>'no index.json'];
 	$tPage=file_get_contents('page.html');
 	$json=file_get_contents("$d/$f.json");
 	$content=json_decode($json,true);
@@ -32,7 +53,7 @@ function createPage($d,$f)
 	}
 	$content['entries']=$entries;
 	replace($tPage,$content);
-	return $tPage;
+	return ['page'=>$tPage];
 }
 
 function replace(&$content,$replacements)
